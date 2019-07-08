a2d72d29ef04c5aedc261e9332878e5a08e9429d
haeyoon <haeyoon.chang@gmail.com>
2019-07-08 10:19:29 -0700

    Code related to options added to Project1.java

--- apptbook/src/main/java/edu/pdx/cs410J/haeyoon/Project1.java
+++ apptbook/src/main/java/edu/pdx/cs410J/haeyoon/Project1.java
@@ -5,13 +5,37 @@ package edu.pdx.cs410J.haeyoon;
  */
 public class Project1 {
 
-  public static void main(String[] args) {
-    Appointment appointment = new Appointment();  // Refer to one of Dave's classes so that we can be sure it is on the classpath
-    System.err.println("Missing command line arguments");
-    for (String arg : args) {
-      System.out.println(arg);
+  /**
+   * Main program that parses the command line, create
+   * <code>Appointment</code> and <code>AppointmentBook</code>,
+   * and prints a description of the appointment to
+   * standard out if -print flag is invoked.
+   */
+  public static void main(String[] args){
+
+    if(args.length == 0){
+      System.err.println("Missing command line arguments");
+      System.exit(1);
     }
-    System.exit(1);
+
+    Appointment appointment0 = new Appointment();
+    AppointmentBook book0 = new AppointmentBook();
+
+
+    String option = args[0];
+
+    if(option.equals("-README")){
+      System.out.println("Here is project description.");
+      System.exit(0);
+    }else if(option.equals("-print")){
+      String s = appointment0.getDescription();
+      System.out.print(s);
+    }
+
+    //for (String arg: args){
+    //  System.out.println(arg);
+    //}
+
   }
 
 }
