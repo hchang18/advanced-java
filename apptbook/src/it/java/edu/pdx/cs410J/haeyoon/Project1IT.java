31a66bfa2404acb794339412d63c2b229d7883e4
haeyoon <haeyoon.chang@gmail.com>
2019-07-08 12:17:45 -0700

    More command line parsing related tests added to Project1.IT

--- apptbook/src/it/java/edu/pdx/cs410J/haeyoon/Project1IT.java
+++ apptbook/src/it/java/edu/pdx/cs410J/haeyoon/Project1IT.java
@@ -14,7 +14,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Integration tests for the {@link Project1} main class.
  */
 public class Project1IT extends InvokeMainTestCase {
-
   /**
    * Invokes the main method of {@link Project1} with the given arguments.
    */
@@ -22,6 +21,12 @@ public class Project1IT extends InvokeMainTestCase {
     return invokeMain( Project1.class, args );
   }
 
+  private String owner = "Pete the Cat";
+  private String desc = "birthday party";
+  private String beginDate = "12/11/2018";
+  private String beginTime = "12:01";
+  private String endDate = "12/13/2018";
+  private String endTime = "2:36";
   /**
    * Tests that invoking the main method with no arguments issues an error
    */
@@ -38,30 +43,64 @@ public class Project1IT extends InvokeMainTestCase {
    */
   @Test
   public void whenREADMEIsFlaggedPrintProjectDescAndExit(){
-    MainMethodResult result = invokeMain(Project1.class, "-README", "aa", "bb");
+    MainMethodResult result = invokeMain(Project1.class, "-README", owner, desc, beginDate);
     assertThat(result.getTextWrittenToStandardOut(), containsString("Here is project description"));
     assertThat(result.getExitCode(), equalTo(0));
   }
 
-  /**
-   * Test that invoking the main method with arguments that contains -print
-   * create Appointment with description and
-   * prints out the appointment description
-   */
   @Test
-  public void whenPrintIsFlaggedCreateAppointmentAndPrintAppointmentDesc(){
-    MainMethodResult result = invokeMain(Project1.class, "-print", "aa", "bb");
-    assertThat(result.getTextWrittenToStandardOut(), equalTo("This method is not implemented yet"));
-
+  public void whenPrintIsFlaggedAndREADMEIsFlaggedPrintProjectDescAndExit(){
+    MainMethodResult result = invokeMain(Project1.class, "-print", "-README", owner, desc, beginDate);
+    assertThat(result.getTextWrittenToStandardOut(), containsString("Here is project description"));
+    assertThat(result.getExitCode(), equalTo(0));
   }
 
 
+  /**
+   * Tests that invoking the main method with arguments that contains -print
+   */
+  @Test
+  public void whenPrintIsFlaggedAndREADMEIsNotFlaggedAndArgsAreLessThan7PrintMissingArgumentsToStandardError(){
+    MainMethodResult result = invokeMain(Project1.class, "-print", owner, desc);
+    assertThat(result.getTextWrittenToStandardError(), containsString("Missing arguments"));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
 
-  // number of args should be 6
+  @Test
+  public void whenPrintIsFlaggedAndREADMEIsNotFlaggedAndArgsAreMoreThan7PrintExtraneousArgumentsToStandardError(){
+    MainMethodResult result = invokeMain(Project1.class, "-print", owner, desc, beginDate, beginTime, endDate, endTime, "hello");
+    assertThat(result.getTextWrittenToStandardError(), containsString("Extraneous arguments"));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
 
-  // when there is -print in args, the number of arguments should be 7
+  @Test
+  public void whenPrintIsFlaggedAndREADMEIsNotFlaggedAndArgsAreExactly7CreateAppointmentAndPrintAppointmentDesc(){
+    MainMethodResult result = invokeMain(Project1.class, "-print", owner, desc, beginDate, beginTime, endDate, endTime);
+    assertThat(result.getTextWrittenToStandardOut(), containsString("This method is not implemented yet"));
+  }
 
-  // Appointment is added to the AppointmentBook (maybe in AppointmentTest?)
+  /**
+   * Tests that invoking the main method with no options but arguments
+   */
+  @Test
+  public void whenNoOptionIsFlaggedAndArgsAreLessThan6PrintMissingArgumentsToStandardError(){
+    MainMethodResult result = invokeMain(Project1.class, owner , desc);
+    assertThat(result.getTextWrittenToStandardError(), containsString("Missing arguments"));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+
+  @Test
+  public void whenNoOptionIsFlaggedAndArgsAreMoreThan6PrintExtraneousArgumentsToStandardError(){
+    MainMethodResult result = invokeMain(Project1.class, owner, desc, beginDate, beginTime, endDate, endTime, "hello");
+    assertThat(result.getTextWrittenToStandardError(), containsString("Extraneous arguments"));
+    assertThat(result.getExitCode(), equalTo(1));
+  }
+
+  @Test
+  public void whenNoOptionIsFlaggedAndArgsAreExactly6CreateAppointmentAndAppointmentBook(){
+    MainMethodResult result = invokeMain(Project1.class, owner, desc, beginDate, beginTime, endDate, endTime);
+    assertThat(result.getExitCode(), equalTo(0));
+  }
 
 
 }
