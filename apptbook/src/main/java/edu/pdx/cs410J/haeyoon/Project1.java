1cdb02921fe5ddfed8309da93b1fea9c92a03e13
haeyoon <haeyoon.chang@gmail.com>
2019-07-08 12:20:51 -0700

    Code added to handle different command line arguments

--- apptbook/src/main/java/edu/pdx/cs410J/haeyoon/Project1.java
+++ apptbook/src/main/java/edu/pdx/cs410J/haeyoon/Project1.java
@@ -13,23 +13,62 @@ public class Project1 {
    */
   public static void main(String[] args){
 
+    //Check if any arguments are passed in
     if(args.length == 0){
       System.err.println("Missing command line arguments");
       System.exit(1);
     }
 
-    Appointment appointment0 = new Appointment();
-    AppointmentBook book0 = new AppointmentBook();
 
-
-    String option = args[0];
-
-    if(option.equals("-README")){
+    //If first argument is -README,
+    //print out project description and exit
+    if(args[0].equals("-README")){
       System.out.println("Here is project description.");
       System.exit(0);
-    }else if(option.equals("-print")){
-      String s = appointment0.getDescription();
-      System.out.print(s);
+    }
+
+
+    //If first argument is -print,
+    //check the second argument is -README ?
+    //print out proj desc : # of args should be 7
+    if(args[0].equals("-print")){
+
+      if(args[1].equals("-README")){
+        System.out.println("Here is project description.");
+        System.exit(0);
+      }
+
+      if(args.length < 7){
+        System.err.println("Missing arguments");
+        System.exit(1);
+      } else if (args.length > 7){
+        System.err.println("Extraneous arguments");
+        System.exit(1);
+      } else {
+        // create Appointment and Appointment Book classes
+        Appointment newAppointment = new Appointment();
+        AppointmentBook newAppointmentBook = new AppointmentBook();
+        // print out appointment description
+        String s = newAppointment.getDescription();
+        System.out.println(s);
+      }
+
+    } else {
+
+      if(args.length < 6){
+        System.err.println("Missing arguments");
+        System.exit(1);
+      } else if (args.length > 6){
+        System.err.println("Extraneous arguments");
+        System.exit(1);
+      } else {
+         // create Appointment and Appointment Book classes
+        Appointment newAppointment = new Appointment();
+        AppointmentBook newAppointmentBook = new AppointmentBook();
+        System.exit(0);
+      }
+
+
     }
 
     //for (String arg: args){
