2d42963558a69238e3f11e10bab13074d5bc4b15
haeyoon <haeyoon.chang@gmail.com>
2019-07-08 10:18:39 -0700

    Tests added to Project1IT.java

--- apptbook/src/it/java/edu/pdx/cs410J/haeyoon/Project1IT.java
+++ apptbook/src/it/java/edu/pdx/cs410J/haeyoon/Project1IT.java
@@ -3,6 +3,9 @@ package edu.pdx.cs410J.haeyoon;
 import edu.pdx.cs410J.InvokeMainTestCase;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -23,10 +26,42 @@ public class Project1IT extends InvokeMainTestCase {
    * Tests that invoking the main method with no arguments issues an error
    */
   @Test
-  public void testNoCommandLineArguments() {
+  public void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
     MainMethodResult result = invokeMain();
     assertThat(result.getExitCode(), equalTo(1));
     assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
   }
+
+  /**
+   * Tests that invoking the main method with arguments that contains -README
+   * prints out the project description and exit
+   */
+  @Test
+  public void whenREADMEIsFlaggedPrintProjectDescAndExit(){
+    MainMethodResult result = invokeMain(Project1.class, "-README", "aa", "bb");
+    assertThat(result.getTextWrittenToStandardOut(), containsString("Here is project description"));
+    assertThat(result.getExitCode(), equalTo(0));
+  }
+
+  /**
+   * Test that invoking the main method with arguments that contains -print
+   * create Appointment with description and
+   * prints out the appointment description
+   */
+  @Test
+  public void whenPrintIsFlaggedCreateAppointmentAndPrintAppointmentDesc(){
+    MainMethodResult result = invokeMain(Project1.class, "-print", "aa", "bb");
+    assertThat(result.getTextWrittenToStandardOut(), equalTo("This method is not implemented yet"));
+
+  }
+
+
+
+  // number of args should be 6
+
+  // when there is -print in args, the number of arguments should be 7
+
+  // Appointment is added to the AppointmentBook (maybe in AppointmentTest?)
+
 
 }
