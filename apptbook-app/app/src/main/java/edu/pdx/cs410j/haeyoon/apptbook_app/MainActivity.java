cd03ae06e85f983c2d7867b0b87a63280f3f4a08
haeyoon <haeyoon.chang@gmail.com>
2019-08-12 03:28:03 -0700

    added all main files

---
+++ apptbook-app/app/src/main/java/edu/pdx/cs410j/haeyoon/apptbook_app/MainActivity.java
@@ -0,0 +1,41 @@
+package edu.pdx.cs410j.haeyoon.apptbook_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button createAppt = (Button) findViewById(R.id.create_appointment_button);
+        Button searchAppt = (Button) findViewById(R.id.search_appointment_button);
+        Button viewApptBook = (Button) findViewById(R.id.view_appointment_book_button);
+        Button helpButton = (Button) findViewById(R.id.help_button);
+
+    }
+
+    public void onHelpClick(View view) {
+
+        Intent getHelpScreenIntent = new Intent(this, Readme.class);
+
+        startActivity(getHelpScreenIntent);
+
+    }
+
+    public void onCreateApptClick(View view) {
+    }
+
+    public void onSearchApptClick(View view) {
+    }
+
+    public void onViewApptBookClick(View view) {
+    }
+}
