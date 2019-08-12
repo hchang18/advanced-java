5f0b209ddc15b23cc97554afcaf56334dd3a5a96
haeyoon <haeyoon.chang@gmail.com>
2019-08-12 03:22:55 -0700

    Help button done

---
+++ apptbook-app/app/src/main/java/edu/pdx/cs410j/haeyoon/apptbook_app/Readme.java
@@ -0,0 +1,26 @@
+package edu.pdx.cs410j.haeyoon.apptbook_app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Readme extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.readme_layout);
+
+    }
+
+    public void onReturnHome(View view) {
+
+        Intent goingBack = new Intent();
+
+        setResult(RESULT_OK, goingBack);
+        finish();
+
+    }
+}
